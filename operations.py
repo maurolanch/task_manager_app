873a44ea5c6d8af79d3746f7e231e39d5cf67277
Mauricio L <maurolanm@gmail.com>
2025-06-05 15:57:40 -0500

    read task by id completed

--- operations.py
+++ operations.py
@@ -17,3 +17,11 @@ def read_all_tasks() -> list[TaskWithID]:
         return [TaskWithID(**row) for row in reader]
     
 
+def read_task(task_id: int) -> Optional[TaskWithID]:
+    with open(DATABASE_FILENAME) as csvfile:
+        reader = csv.DictReader(
+            csvfile
+        )
+        for row in reader:
+            if int(row["id"]) == task_id:
+                return TaskWithID(**row)
