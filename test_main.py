3f7a0dc866371d682be1a697008117ed87cd95f0
Mauricio L <maurolanm@gmail.com>
2025-06-06 17:26:47 -0500

    test main delete func completed

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 from main import app
 from fastapi.testclient import TestClient
 
-client = TestClient()
+client = TestClient(app)
 
 from conftest import TEST_TASKS
 
@@ -35,3 +35,31 @@ def test_endpoint_create_task():
     assert response.json() == {**task, "id":3}
     assert len(read_all_tasks()) == 3
 
+from operations import read_task
+
+def test_endpoint_modify_task():
+    updated_fields = {"status":"Finished"}
+    response =  client.put("/task/2", json=updated_fields)
+
+    assert response.status_code == 200
+    assert response.json() == {
+        **TEST_TASKS[1],
+        **updated_fields
+    }
+
+    response = client.put(
+        "/test/3", json=updated_fields
+    )
+
+    assert response.status_code == 404
+
+
+def test_endpoint_delete_task():
+    response = client.delete("/task/2")
+    assert response.status_code == 200
+
+    expected_response = TEST_TASKS[1]
+    del expected_response["id"]
+
+    assert response.json() == expected_response
+    assert read_task(2) is None
