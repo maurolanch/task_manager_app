3a948ce44a9fcfe8f5ce248b82f27cf90046e04b
Mauricio L <maurolanm@gmail.com>
2025-06-06 16:24:48 -0500

    test_main read all tasks and get_task

--- test_main.py
+++ test_main.py
@@ -10,3 +10,12 @@ def test_endpoint_read_all_tasks():
     assert response.status_code == 200
     assert response.json() == TEST_TASKS
 
+
+def test_endpoint_get_task():
+    response = client.get("/task/1")
+
+    assert response.status_code == 200
+    assert response.json() == TEST_TASKS[0]
+    response = client.get("/task/5")
+
+    assert response.status_code == 404
